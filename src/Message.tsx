d0dcb765bfe69c082a43ccd5254a1afafd31dba8
subhav-des <subhav.des@gmail.com>
2025-08-28 23:15:04 +0530

    Added github message

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,6 @@
 function Message() {
     //JSX: Javascript XML
-    return <h1>Hey! You actually created a component?</h1>;
+    return <h1>Hey! You actually created a component?<br /> I also used Github</h1>;
 }
 
 export default Message;
